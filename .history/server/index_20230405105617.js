e6d6e1f1b42923149ce5eacb65d5301dafe54733
HenrryWith2Rs <50603179+HenrryWith2Rs@users.noreply.github.com>
2023-04-05 11:36:13 -0400

    Initial Commit
    Monolithic App
    SERVER:
    packages / dependencies: cors dotevn express mongoose nodemon
    connected to mongoDB, needs url in .env file
    
    CLIENT:
    react app created in vite, all fluff removed, bootstrap linked in index.html file

---
+++ .history/server/index_20230405105617.js
@@ -0,0 +1,26 @@
+// external imports
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+// internal imports
+import connectDB from './mongodb/connect.js';
+
+dotenv.config();
+
+// middleware
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+// confirm app is running
+app.get('/', async (req, res) => {
+  res.send('Hello from Ramos');
+});
+
+const port = 7777;
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+  } catch (error) {}
+};
---
+++ .history/server/index_20230405105706.js
@@ -0,0 +1,29 @@
+// external imports
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+// internal imports
+import connectDB from './mongodb/connect.js';
+
+dotenv.config();
+
+// middleware
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+// confirm app is running
+app.get('/', async (req, res) => {
+  res.send('Hello from Ramos');
+});
+
+const port = 7777;
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(port, () => console.log('Server has started on port ' + port));
+  } catch (error) {
+    console.log(error);
+  }
+};
